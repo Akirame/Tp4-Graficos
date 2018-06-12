2686d8a01e5f5706d3122c5e76fa42c1a8e3c479
Unknown <jquintanamartin@hotmail.com>
2018-06-12 15:35:46 -0300

    Proyecto + Clases

---
+++ HighScoreLib/HighScoreLib/Score.cpp
@@ -0,0 +1,14 @@
+#include "Score.h"
+
+
+
+Score::Score()
+{
+	name = " ";
+	score = NULL;
+}
+
+
+Score::~Score()
+{
+}
---
+++ HighScoreLib/HighScoreLib/Score.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Score
+{
+public:
+	Score();
+	~Score();
+private:
+	string name;
+	int score;
+};
+
